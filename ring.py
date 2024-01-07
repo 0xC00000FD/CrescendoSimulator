dbd5153bbeecde3fe2da5180b4eb0341ff7973ae
0xC00000FD <floarea.raul@gmail.com>
2024-01-07 12:50:33 +0200

    collision detection system

--- ring.py
+++ ring.py
@@ -1,5 +1,5 @@
 import vector
-from math import sin, cos, pi
+from math import sin, cos, pi, radians
 from time import time
 
 AIR_DENSITY = 1.293 # kg / (m^(-3))
@@ -20,12 +20,13 @@ class Ring:
         self.lastTime = 0
         
     #Provide angle in Radians and ringSpeed in m/s
-    def shoot(self, angle, ringSpeed):
+    def shoot(self, angle, ringSpeed, deltaX):
         self.position = vector.obj(x = 0, y = 0, z = 0)
         self.angle = angle
         self.velocity = vector.obj(rho = ringSpeed, phi = angle)
         self.startTime = time()
         self.lastTime = time()
+        self.deltaX = deltaX
     
     def update(self):
         currTime = time()
@@ -33,11 +34,42 @@ class Ring:
         
         self.position += self.velocity * deltaTime
         
-        drag = - 1 / 2 * self.dragCoefficient * AIR_DENSITY * self.getArea() * self.velocity * self.velocity.rho
+        drag = - 1 / 2 * self.dragCoefficient * AIR_DENSITY * self.getArea() * self.velocity * self.velocity.rho 
         
-        self.velocity += (self.acceleration + drag) * deltaTime
+        self.velocity += (self.acceleration + drag) / self.mass * deltaTime
                                 
         self.lastTime = currTime
+        
+    def checkCollisions(self):
+        E = vector.obj(rho = self.outerDiameterMeters, phi = self.angle) # vector E
+        A = self.position - E / 2 # vector A
+            
+        C = vector.obj(x = self.deltaX, y = 0)
+        F =  vector.obj(rho = 1.9812, phi = radians(90))
+        
+        (h, g) = self.calculateHG(A, E, C, F)
+                
+        if 0 <= h and h <= 1 and 0 <= g and g <= 1:
+            return True
+        
+        C =  vector.obj(x = self.deltaX - 0.4572, y = 2.0828)
+        F =  vector.obj(rho = 500, phi = radians(90))
+        
+        (h, g) = self.calculateHG(A, E, C, F)
+        
+        if 0 <= h and h <= 1 and 0 <= g and g <= 1:
+            return True
+        
+        return False
+        
+    def calculateHG(self, A : vector.VectorObject2D, E : vector.VectorObject2D, C : vector.VectorObject2D, F : vector.VectorObject2D):
+        P = vector.obj(rho = E.rho, phi = E.phi + pi / 2)
+        Q = vector.obj(rho = F.rho, phi = F.phi + pi / 2)
+        
+        h = ((A - C).dot(P)) / (F.dot(P))
+        g = ((C - A).dot(Q)) / (E.dot(Q))
+        
+        return (h, g)
         
     def addForce(self, forceVector):
         self.acceleration += forceVector
--- simulator.py
+++ simulator.py
@@ -3,19 +3,30 @@ from math import radians
 import vector
 import matplotlib.pyplot as plt
 
+def closest(lst, K):
+     return lst[min(range(len(lst)), key = lambda i: abs(lst[i]-K))]
+
+distance = 1 # m
+speed = 25 #m / s
+angle = radians(75) # rad
+
 ring1 = Ring(0.12, 0.253, 1, 0.254, 0.3556, 0.0508)
-ring1.shoot(radians(45), 15)
+ring1.shoot(angle, speed, distance)
 ring1.addForce(vector.obj(x = 0.0, y = -9.81))
 
-ringPosition = []
-timeStamps = []
+ringHeights = []
+ringXPos = []
 
-while ring1.getPosition().y >= 0:    
-    ringPosition.append(ring1.getPosition().y)
-    timeStamps.append(ring1.getPosition().x)
-    
+while ring1.getPosition().y >= 0 and not ring1.checkCollisions():    
+    ringHeights.append(ring1.getPosition().y)
+    ringXPos.append(ring1.getPosition().x)
+        
     ring1.update()
     
-print(max(ringPosition))
-plt.scatter(timeStamps, ringPosition)
+print(max(ringHeights))
+
+ringHeights[ringXPos.index(closest(ringXPos, distance))] = 1.9812
+
+plt.scatter(ringXPos, ringHeights)
 plt.show()
+
