367dd5e0d4672d6d5fe64a34f44d39f2261a0da8
0xC00000FD <floarea.raul@gmail.com>
2024-01-07 15:54:56 +0200

    WIP Simulation Code

--- ring.py
+++ ring.py
@@ -18,15 +18,17 @@ class Ring:
         self.position = vector.obj(x = 0, y = shooterHeightMeters)
         self.angle = 0
         self.lastTime = 0
+        self.closestDistance = 0
         
     #Provide angle in Radians and ringSpeed in m/s
     def shoot(self, angle, ringSpeed, deltaX):
-        self.position = vector.obj(x = 0, y = 0, z = 0)
+        self.position = vector.obj(x = 0, y = self.shooterHeightMeters)
         self.angle = angle
         self.velocity = vector.obj(rho = ringSpeed, phi = angle)
         self.startTime = time()
         self.lastTime = time()
         self.deltaX = deltaX
+        self.closestDistance = 0
     
     def update(self):
         currTime = time()
@@ -45,12 +47,12 @@ class Ring:
         A = self.position - E / 2 # vector A
             
         C = vector.obj(x = self.deltaX, y = 0)
-        F =  vector.obj(rho = 1.9812, phi = radians(90))
+        F = vector.obj(rho = 1.9812, phi = radians(90))
         
         (h, g) = self.calculateHG(A, E, C, F)
                 
         if 0 <= h and h <= 1 and 0 <= g and g <= 1:
-            return True
+            return 1
         
         C =  vector.obj(x = self.deltaX - 0.4572, y = 2.0828)
         F =  vector.obj(rho = 500, phi = radians(90))
@@ -58,9 +60,17 @@ class Ring:
         (h, g) = self.calculateHG(A, E, C, F)
         
         if 0 <= h and h <= 1 and 0 <= g and g <= 1:
-            return True
+            return 2
         
-        return False
+        C = vector.obj(x = self.deltaX, y = 1.9812)
+        F = vector.obj(x = self.deltaX - 0.4572, y = 2.0828)
+        
+        self.closestDistance = min(self.closestDistance, ((C - self.position) + (F - self.position)).rho / 2)
+        
+        if (C - self.position).rho < (F - self.position).rho:
+            return -1
+        else:
+            return -2
         
     def calculateHG(self, A : vector.VectorObject2D, E : vector.VectorObject2D, C : vector.VectorObject2D, F : vector.VectorObject2D):
         P = vector.obj(rho = E.rho, phi = E.phi + pi / 2)
@@ -85,3 +95,6 @@ class Ring:
     
     def getArea(self):
         return 4 * pi * pi * (self.innerDiameterMeters / 2) * (self.outerDiameterMeters / 2)
+    
+    def getClosestDistance(self):
+        return self.closestDistance
--- simulator.py
+++ simulator.py
@@ -1,32 +1,58 @@
 from ring import Ring
-from math import radians
+from math import radians, pi
 import vector
 import matplotlib.pyplot as plt
 
 def closest(lst, K):
-     return lst[min(range(len(lst)), key = lambda i: abs(lst[i]-K))]
+    return lst[min(range(len(lst)), key = lambda i: abs(lst[i]-K))]
 
-distance = 1 # m
-speed = 25 #m / s
-angle = radians(75) # rad
+minDistance = 1 # m
+speed = 0 # m / s
+angle = radians(0) # rad
+theoreticalMaxRps = 6784 / 60 # Rotations / s
+wheelRadius = 0.0508 # meters
+theoreticalMaxSpeed = theoreticalMaxRps * 2 * pi * wheelRadius # m / s
 
 ring1 = Ring(0.12, 0.253, 1, 0.254, 0.3556, 0.0508)
-ring1.shoot(angle, speed, distance)
-ring1.addForce(vector.obj(x = 0.0, y = -9.81))
+ring1.addForce(vector.obj(x = 0.0, y = -9.81)) #gravitational force
 
-ringHeights = []
-ringXPos = []
-
-while ring1.getPosition().y >= 0 and not ring1.checkCollisions():    
-    ringHeights.append(ring1.getPosition().y)
-    ringXPos.append(ring1.getPosition().x)
+class Solution:
+    def __init__(self, distance, speed, angle, rating):
+        self.distance = distance
+        self.speed = speed
+        self.angle = angle
+        self.rating = rating
         
-    ring1.update()
-    
-print(max(ringHeights))
+solutions = []
 
-ringHeights[ringXPos.index(closest(ringXPos, distance))] = 1.9812
+checkCollisions = 0
+for distance in range(minDistance, 4, 1):
+    for angle in range(75, 0, -1):
+        st = 1
+        dr = theoreticalMaxSpeed
+        
+        while st <= dr:
+            speed = (st + dr) / 2
+            
+            ring1.shoot(radians(angle), speed, distance)
+            
+            while ring1.getPosition().y >= 0:                    
+                checkCollisions = ring1.checkCollisions()
+                if checkCollisions > 0:
+                    break
+                
+                ring1.update()
+                        
+            if checkCollisions == 1 or ring1.getPosition().y < 0:
+                st = speed + 0.5
+            elif checkCollisions == 2:
+                dr = speed - 0.05
+            else:
+                if checkCollisions == -1:
+                    solutions.append(Solution(distance, speed, radians(angle), ring1.getClosestDistance))
+                    st = speed + 0.5
+                else:
+                    solutions.append(Solution(distance, speed, radians(angle), ring1.getClosestDistance))
+                    dr = speed - 0.05
 
-plt.scatter(ringXPos, ringHeights)
-plt.show()
-
+print(solutions)
